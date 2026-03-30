fd3d84e63204002c82b95ae4534f90d06a3c1b85
TymekBrunka <tymkopolo@wp.pl>
2026-03-30 09:57:40 +0200

    -

--- src/Main.hx
+++ src/Main.hx
@@ -1,8 +1,12 @@
+import hxd.Key;
 import hxd.res.Loader;
 import hxd.fs.LocalFileSystem;
 
 class Main extends hxd.App {
-	public var tile:h2d.Bitmap;
+	public var mainTexture:h2d.Tile;
+	public var sprites:h2d.SpriteBatch.SpriteBatch;
+	public var spaceship:h2d.SpriteBatch.BasicElement;
+	// public var playerObj:h2d.Object;
 
 	override function init() {
 		super.init();
@@ -12,20 +16,41 @@ class Main extends hxd.App {
 		tf.text = "Hello World\nHeaps is great!";
 		s2d.addChild(tf);
 
-		tile = new h2d.Bitmap(hxd.Res.textures.spaceship.toTile().center());
-		tile.tile.center();
-		tile.width = 64;
-		tile.height = 64;
-		s2d.addChild(tile);
+		mainTexture = hxd.Res.textures.spaceship.toTile();
+		sprites = new h2d.SpriteBatch.SpriteBatch(mainTexture);
+		sprites.hasRotationScale = true;
+		sprites.hasUpdate = true;
+
+		// playerObj = new h2d.Object();
+		// s2d.addChild(playerObj);
+
+		spaceship = new h2d.SpriteBatch.BasicElement(mainTexture.center());
+		spaceship.scale = 5;
+		spaceship.rotation = 1.57;
+		spaceship.x = 500;
+		spaceship.y = 500;
+		// spaceship.vx = 0.5;
+		// spaceship.vy = 0.5;
+		sprites.add(spaceship);
+		s2d.addChild(sprites);
 	}
 
 	override function update(dt:Float) {
-		tile.x = engine.width / 2;
-		tile.y = engine.height / 2;
+		s2d.camera.x = spaceship.x;
+		s2d.camera.y = spaceship.y;
+		// s2d.camera.follow = playerObj;
+		s2d.camera.anchorX = 0.5;
+		s2d.camera.anchorY = 0.5;
 	}
 
 	static function main() {
-		hxd.Res.loader = new Loader(new LocalFileSystem("assets", null, "assets"));
+		// if (!hxd.tools.RenderDoc.init())
+		// 	throw "Can't init RenderDoc, renderdoc.dll may be missing from your PATH!";
+		// hxd.tools.RenderDoc.launchReplayUi(true, null);
+		// hxd.tools.RenderDoc.setCaptureKeys([Key.F11]);
+
+		// hxd.Res.loader = new Loader(new LocalFileSystem("assets", null, "assets"));
+		hxd.Res.initLocal();
 		new Main();
 	}
 }
